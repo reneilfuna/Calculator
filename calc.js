78728057f4bffc3842d1878df29206c9ea935ee0
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-27 12:30:24 +0000

    Build add function

---
+++ calc.js
@@ -0,0 +1,13 @@
+
+// Create add function
+function add(val1, val2){
+    if (typeof(val1) != "number" || typeof(val2) != "number")
+        return "Error";
+    else return val1 + val2;
+}
+
+// Create subtract function
+
+// Create multiply function
+
+// Create divide function
