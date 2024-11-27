a90a9801941487cc19ff4d3523a800d10e6c2edb
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-27 12:34:52 +0000

    Build subtract function

--- calc.js
+++ calc.js
@@ -7,7 +7,11 @@ function add(val1, val2){
 }
 
 // Create subtract function
-
+function subtract(val1, val2){
+    if (typeof(val1) != "number" || typeof(val2) != "number")
+        return "Error";
+    else return val1 - val2;
+}
 // Create multiply function
 
 // Create divide function
