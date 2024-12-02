a3795c2d9d21f7f754d7705b787f0e2e5cc9c472
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-12-02 16:32:32 +0000

    Enable simple number inputs

--- calc.js
+++ calc.js
@@ -55,148 +55,32 @@ function operate(val1, oper, val2){
 
 const btnContainer = document.getElementById("buttons");
 
-// for (i=0; i<5; i++) {
-//     const buttonRow = document.createElement("div");
-//     buttonRow.classList.add("rowOfButtons");
-
-//     if (i==4){
-//         for(j=0; j<3; j++){
-//             const btn = document.createElement("div");
-//             btn.classList.add("button");
-//             if (j==0) btn.classList.add("zero");
-//             if (j==2) btn.classList.add("end");
-//             buttonRow.appendChild(btn);
-//         }
-//     }
-//     else {
-//         for (j=0; j<4; j++){
-//             const btn = document.createElement("div");
-//             btn.classList.add("button");
-//             if (j==3) btn.classList.add("end");
-//             else if (i==0)btn.classList.add("top");
-//             buttonRow.appendChild(btn);
-//         }
-//     }
-//     btnContainer.appendChild(buttonRow);
-//     console.log("row appended!");
-// }
-
-// const btnContents = ["C", "+/-", "%", "\xf7",
-//     "7", "8", "9", "\xD7",
-//     "4", "5", "6", "-",
-//     "1", "2", "3", "+",
-//     "0", ".", "="
-// ];
-
-// const buttonList = document.querySelectorAll(".button");
-// for(i=0; i<buttonList.length; i++){
-//     buttonList[i].textContent = btnContents[i];
-//     buttonList[i].setAttribute("id", `${btnContents[i]}`)
-// }
-
-
 const display = document.getElementById("display");
 
-function inputNum(input){
+function numberHandler(number){
     // Prevent overflow
     if (!(display.textContent.length == 9)){
-        if (input == "0"){
-            if (!(display.textContent == "0")) display.textContent += "0";
+        // First digit 
+        if (display.textContent == "0" && number != 0){
+            display.textContent = number;
+        } // Succeeding digits
+        else {
+            display.textContent += number;
         }
-        // Prevent preceding zeros
-        else if (display.textContent == 0) display.textContent = input;
-        else display.textContent += input;
+        
     }
 }
 
-// Function stores the first value and the operator
-function storeOper(input, oper){
-    // Store val1 
-    // Store operator
-    num1 = input;
-    operator = oper;
-    console.log(`Num1: ${num1} oper: ${operator}`)
-    // Clear display
-    display.textContent = "0";
-}
-
-// function callOper(input){
-//     console.log(`num1: ${num1}, oper: ${operator} num2: ${input}`)
-//     return operate(num1, operator, input);
-// }
-
 btnContainer.addEventListener("click", (event) => {
     let target = event.target;
-    switch (target.textContent){
-        case "+":
-            storeOper((display.textContent), "+")
-            break;    
-        case "-":
-            console.log("my fav is proabably jollibee tbh");
-            break;
-        case "\xf7":
-            console.log("DOGGY GAME I NEVER WANTED");
-            break;
-        case "\xD7":
-            console.log("men 97 is a brilliant animated series.");
-            break;
-        case "=":
-            // Call operate function
-            display.textContent = operate(num1, operator, display.textContent);
-            break;
-        case ".":
-            console.log("mr k - is cooking");
-            break;
-        case "0":
-            console.log("0");
-            inputNum("0");
-            break;       
-        case "1":
-            console.log("1");
-            inputNum("1");
-            break;
-        case "2":
-            console.log("2");
-            inputNum("2");
-            break;
-        case "3":
-            console.log("3");
-            inputNum("3");
-            break;
-        case "4":
-            console.log("4");
-            inputNum("4");
-            break;
-        case "5":
-            console.log("5");
-            inputNum("5");
-            break;
-        case "6":
-            console.log("6");
-            inputNum("6");
-            break;
-        case "7":
-            console.log("7");
-            inputNum("7");
-            break;
-        case "8":
-            console.log("8");
-            inputNum("8");
-            break;
-        case "9":
-            console.log("");
-            inputNum("9");
-            break;
-        case "C":
-            console.log("0");
-            display.textContent = "0";
-            break;    
-        case "+/-":
-            console.log("CHANGE SIGN BRO");
-            break;
-        case "%":
-            console.log("ALL ABOUT THE PERCENTAGES");
-            break;    
+    if (target.className.includes("number")){
+        numberHandler(target.textContent)
+    }
+    else if(target.className.includes("operator")){
+        console.log("le noomber jacks");
+    }
+    else if (target.className.includes("alt")){
+        console.log("youve gut a moostash")
     }
 });                                                               
         
