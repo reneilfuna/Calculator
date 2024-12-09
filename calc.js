f64344d96830e95cb366c21de0df758e24b0ff7f
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-12-09 14:38:23 +0000

    Delete original operator functions

--- calc.js
+++ calc.js
@@ -30,33 +30,6 @@ function roundDisplay(value){
 
 }
 
-// // Create add function
-// function add(val1, val2){
-//     if (typeof(val1) != "number" || typeof(val2) != "number")
-//         return "Error";
-//     else return roundDisplay(parseFloat(val1) + parseFloat(val2));
-// }
-
-// // Create subtract function
-// function subtract(val1, val2){
-//     if (typeof(val1) != "number" || typeof(val2) != "number")
-//         return "Error";
-//     else return roundDisplay(parseFloat(val1) - parseFloat(val2));
-// }
-// // Create multiply function
-// function multiply(val1, val2){
-//     if (typeof(val1) != "number" || typeof(val2) != "number")
-//         return "Error";
-//     else return roundDisplay(parseFloat(val1) * parseFloat(val2));
-// }
-
-// // Create divide function
-// function divide(val1, val2){
-//     if (typeof(val1) != "number" || typeof(val2) != "number")
-//         return "Error";
-//     else if (val2 == 0) return "jokeman";
-//     else return roundDisplay(parseFloat(val1) / parseFloat(val2));
-// }
 function calculate(val1, val2, operation){
     // Ensure all inputs are numbers
     if (typeof val1 !== "number" || typeof val2 !== "number") return "Error";
